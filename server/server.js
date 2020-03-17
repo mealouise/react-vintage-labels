02e7e5e300d7de9a5abbc9d897a9a9e0b26ff3c4
Emma Sheridan <emmasheridan@Emmas-MacBook-Air.local>
2020-03-17 20:07:04 +0000

    1.1.0

--- server/server.js
+++ server/server.js
@@ -8,4 +8,30 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use
+app.use(cors());
+app.use(bodyParser());
+app.use(express.json());
+
+const uri = process.env.URI;
+mongoose.connect(uri, {useNewParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    useNewUrlParser: true,
+    console.log('mongodb database connection established successfully'); 
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
+
+
+
+app.get('/get-listings', (req,res) => {
+    listingSchema.find(
+        {},
+        (err, listing) => {
+            if (err) return res.render({err:err});
+            res.send(listing)
+        }
+    )
+});
